51400680371ee7fc9a90fea82d3e4ba04e614cd8
victorhenriqc <vic.1601@outlook.com>
2022-08-27 17:00:34 -0300

    add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,20 @@
-import { HeaderContainer } from "./styles";
+import { BadgeCart, BadgeLocation, HeaderActions, HeaderContainer } from "./styles";
+import logo from "../../assets/Logo.png"
+import { MapPin, ShoppingCart } from "phosphor-react";
 
 export function Header(){
     return (
         <HeaderContainer>
-            header
+            <img src={logo} alt="" />
+            <HeaderActions>
+                <BadgeLocation>
+                    <MapPin weight="fill"/>
+                    Porto Alegre, RS
+                </BadgeLocation>
+                <BadgeCart>
+                    <ShoppingCart weight="fill"/>
+                </BadgeCart>
+            </HeaderActions>
         </HeaderContainer>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,58 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+    padding: 32px 160px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+export const HeaderActions = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;                                
+    gap: 12px;
+    height: 38px;
+    padding: 0px;
+`
+
+export const BadgeLocation = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+    width: 143px;
+    /* height: 38px; */
+    gap: 4px;
+    border-radius: 6px;
+    font-family: 'Roboto';
+    font-weight: 400;
+    font-size: 0.875rem;
+    color: ${props => props.theme["purple-dark"]};
+    background-color: ${props => props.theme["purple-light"]};
+
+    svg{
+        width: 22px;
+        height: 22px;
+
+        color: ${props => props.theme.purple};
+    }
+`
+
+export const BadgeCart = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+    gap: 4px;
+    border-radius: 6px;
+    background-color: ${props => props.theme["yellow-light"]};
+
+    svg{
+        width: 22px;
+        height: 22px;
+
+        color: ${props => props.theme["yellow-dark"]}
+    }
+`
