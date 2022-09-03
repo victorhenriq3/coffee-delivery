b6651c294da6e1add85fd940f978f6ddd485a318
victorhenriqc <vic.1601@outlook.com>
2022-09-03 19:02:31 -0300

    add a content to card coffee

--- src/components/CoffeeCard/index.tsx
+++ src/components/CoffeeCard/index.tsx
@@ -1,5 +1,22 @@
-import { Card, CoffeeImage, Content, Tags, Tag } from "./styles";
+import { 
+    Card, 
+    CoffeeImage, 
+    Content, 
+    Tags, 
+    Tag,
+    Title, 
+    Description, 
+    Buy,
+    Price, 
+    Actions, 
+    Currency, 
+    Value,
+    Counter,
+    Count,
+    CartContainer
+} from "./styles";
 import cafe from "../../assets/Americano.png"
+import { Minus, Plus, ShoppingCart } from "phosphor-react";
 
 export function CoffeeCard(){
     return(
@@ -13,6 +30,28 @@ export function CoffeeCard(){
                         TRADICIONAL
                     </Tag>
                 </Tags>
+                <Title> 
+                    Expresso Tradicional
+                </Title>
+                <Description>
+                    O tradicional café feito com água quente e grãos moídos
+                </Description>
+                <Buy>
+                    <Price>
+                        <Currency>R$</Currency>
+                        <Value>9,90</Value>
+                    </Price>
+                    <Actions>
+                        <Counter>
+                            <Minus size={14} weight="bold"/>
+                            <Count>1</Count>
+                            <Plus size={14} weight="bold"/>
+                        </Counter>
+                        <CartContainer>
+                            <ShoppingCart weight="fill" size={22}/>    
+                        </CartContainer>
+                    </Actions>
+                </Buy>
             </Content>
         </Card>
     )
--- src/components/CoffeeCard/styles.ts
+++ src/components/CoffeeCard/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Card = styled.div`
     width: 256px;
-    height: 310px;
+    height: 330px;
     background-color: ${props => props.theme["base-card"]};
     border-radius: 6px 36px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -14,10 +14,9 @@ export const CoffeeImage = styled.div`
     top: -20px;
     left: calc(50% - 120px/2);
     
-
     img{
         width: 120px;
-    height: 120px;
+        height: 120px;
     }
 `
 
@@ -52,4 +51,131 @@ export const Tag = styled.span`
     font-weight: 700;
     font-size: 0.625rem;
     line-height: 130%;
+`
+
+export const Title = styled.h1`
+    position: absolute;
+    height: 26px;
+    left: calc(50% - 190px/2);
+    right: 20px;
+    top: 149px;
+
+    font-family: 'Baloo 2';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 130%;
+    display: flex;
+    align-items: flex-end;
+    text-align: center;
+    color: ${props => props.theme["base-subtitle"]};
+`
+
+export const Description = styled.p`
+    position: absolute;
+    height: 36px;
+    left: 20px;
+    right: 20px;
+    top: 183px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 130%;
+    text-align: center;
+    color: ${props => props.theme["base-label"]};
+`
+
+export const Buy = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px;
+    gap: 29px;
+    position: absolute;
+    height: 38px;
+    left: 24px;
+    right: 24px;
+    bottom: 20px;
+`
+
+export const Price = styled.div`
+    width: 67px;
+    height: 31px;
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+`
+
+const BasePrice = styled.span`
+    text-align: right;
+    font-family: 'Roboto';
+    font-style: normal;
+    line-height: 130%;
+    color: ${props => props.theme["base-text"]};
+`
+
+export const Currency = styled(BasePrice)`
+    font-weight: 400;
+    font-size: 0.875rem;
+    margin-right: 5px;    
+`
+
+export const Value = styled(BasePrice)`
+    font-weight: bold;
+    font-size: 1.375rem;
+`
+
+export const Actions = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 8px;
+    width: 118px;
+`
+
+export const Counter = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+    gap: 4px;
+    border-radius: 6px;
+    background-color: ${props => props.theme["base-button"]};
+    width: 64px;    
+    /* height: 30px; */
+
+    svg{
+        color: #8047F8;
+    }
+`
+
+export const Count = styled.span`
+   font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 130%;
+    text-align: center;
+    margin: 0 4px;
+    color: ${props => props.theme["base-title"]};
+`
+
+export const CartContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+    /* width: 38px; */
+    gap: 8px;
+    background: ${props => props.theme["purple-dark"]};
+    border-radius: 6px;
+
+    svg{
+        color: ${props => props.theme["base-card"]}
+    }
 `
